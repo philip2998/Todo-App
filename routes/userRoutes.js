1b0064106e7f2c50cb257495be00ed05cee02e94
philip2998 <filipgevorgyan@gmail.com>
2023-05-08 19:38:40 +0400

    put protect middlewares inside userRoutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,14 +3,21 @@ import UserController from '../controllers/UserController.js';
 import signup from '../controllers/signup.js';
 import login from '../controllers/login.js';
 import protect from '../controllers/protect.js';
+import checkPermissions from '../middlewares/checkPermissions.js';
 
 const userRouter = express.Router();
 
 userRouter.post('/signup', signup);
 userRouter.post('/login', login);
 
+// Protect all routes that it comes after this middleware
+route.use(protect);
+
 userRouter.patch('/updateMe', protect, UserController.updateMe);
 userRouter.delete('/deleteMe', protect, UserController.deleteMe);
+
+// Protect all routes that it comes after this middleware
+route.use(checkPermissions('admin'));
 
 userRouter
   .route('/')
