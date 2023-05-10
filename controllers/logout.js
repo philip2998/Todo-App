8f7b51c0ca36297353e4c0d28655facdeb01fcad
philip2998 <filipgevorgyan@gmail.com>
2023-05-10 19:48:36 +0400

    for logout functionality making logout controller

---
+++ controllers/logout.js
@@ -0,0 +1,9 @@
+const logout = (req, res) => {
+  res.cookie('jwt', 'loggedout', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+  res.status(200).json({ status: 'success' });
+};
+
+export default logout;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,11 +4,13 @@ import signup from '../controllers/signup.js';
 import login from '../controllers/login.js';
 import protect from '../controllers/protect.js';
 import checkPermissions from '../middlewares/checkPermissions.js';
+import logout from '../controllers/logout.js';
 
 const userRouter = express.Router();
 
 userRouter.post('/signup', signup);
 userRouter.post('/login', login);
+userRouter.get('/logout', logout);
 
 // Protect all routes that it comes after this middleware
 userRouter.use(protect);
